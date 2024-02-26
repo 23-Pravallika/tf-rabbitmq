9abad82cb2a4ad5282eafeaecc770362123ae2ce
23-Pravallika <learngain23@gmail.com>
2024-02-26 13:12:09 +0530

    AutoPush

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 # Creates SPOT Servers
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami           = data.aws_ami.my_ami.id
+  ami           = data.aws_ami.lab-image.id
   instance_type = "t2.micro"
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.allow_rabbitmq.id]
