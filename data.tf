3aee74e7fed1d95a46838614096da3f98ccf6bf9
23-Pravallika <learngain23@gmail.com>
2024-02-26 13:10:21 +0530

    AutoPush

--- data.tf
+++ data.tf
@@ -27,3 +27,11 @@ data "aws_ami" "lab-image" {
 }
 
 
+data "aws_secretsmanager_secret" "secrets" {
+   name   =  "robo/secret"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id     = data.aws_secretsmanager_secret.secrets.id
+}
+
